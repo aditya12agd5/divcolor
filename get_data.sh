5c18f3e4021c295ee88404d3cb8692507bb3b1b8
ardeshp2 <ardeshp2@illinois.edu>
2017-04-27 19:08:06 -0700

    Get lfw images

--- get_data.sh
+++ get_data.sh
@@ -1,2 +1,6 @@
 wget http://vision.cs.illinois.edu/projects/divcolor/data.zip
 unzip data.zip
+rm data.zip
+wget http://vis-www.cs.umass.edu/lfw/lfw-deepfunneled.tgz
+tar -xvzf lfw-deepfunneled.tgz
+mv lfw-deepfunneled data/lfw_images
